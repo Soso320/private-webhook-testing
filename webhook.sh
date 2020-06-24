d3f4f4885064251a9e1fb79726ab7f76f7690770
SosoPlayz <43059015+SosoPlayz@users.noreply.github.com>
2020-06-24 19:45:18 +0300

    Create webhook.sh

---
+++ webhook.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo
+curl -H "Content-Type: application/json" -X POST -d @bruh.json https://discordapp.com/api/webhooks/725358306276671588/KtcV7IRdaQrna0_3jdE-dCQEUIRVQ6exqaLwKtib_0KN34YxIg7lIoLuUNgWehSobvUv
